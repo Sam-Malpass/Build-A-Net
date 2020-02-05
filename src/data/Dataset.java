036ef0683c9aa8da58469b698ab877b9fbaa8b60
Unknown <sam@malpass.me.uk>
2020-02-05 12:07:04 +0000

    Add comments to Dataset

--- src/data/Dataset.java
+++ src/data/Dataset.java
@@ -20,8 +20,14 @@ public abstract class Dataset {
      */
     private ArrayList<String> columnHeaders;
 
+    /**
+     * inputCols holds the indices of all input columns
+     */
     private ArrayList<Integer> inputCols;
 
+    /**
+     * outputCols holds the indices of all output columns
+     */
     private ArrayList<Integer> outputCols;
 
 
@@ -37,11 +43,27 @@ public abstract class Dataset {
         return dataFrame.get(0).size();
     }
 
+    /**
+     * Function numInputs()
+     * <p>
+     *     Return the number of inputs
+     * </p>
+     * @return the size of the inputCols list
+     */
     public int numInputs() {
+        // Return the number of input columns
         return inputCols.size();
     }
 
+    /**
+     * Function numOutputs()
+     * <p>
+     *     Return the number of output columns
+     * </p>
+     * @return the size of the outputCols list
+     */
     public int numOutputs() {
+        // Return the number of output columns
         return outputCols.size();
     }
 
@@ -57,23 +79,50 @@ public abstract class Dataset {
         return dataFrame.size();
     }
 
+    /**
+     * Function getRow()
+     * <p>
+     *     Iterates over all the columns of a given row, adding each value to a new list
+     *     The list is then returned as the row data. Excludes any output columns
+     * </p>
+     * @param rowIndex is the row that is needed to be fetched
+     * @return the row
+     */
     public ArrayList<Double> getRow(int rowIndex) {
+        // Create a new row
         ArrayList<Double> row = new ArrayList<>();
+        // For all columns
         for(int i = 0; i < numAttributes(); i++) {
+            // If the column is not an output column
             if(!outputCols.contains(i)) {
+                // Add the value of the column at row index to the row
                 row.add(dataFrame.get(i).get(rowIndex));
             }
         }
+        // Return the row
         return row;
     }
 
+    /**
+     * Function getRowExpected()
+     * <p>
+     *     Finds the expected output(s) for a given row in the data frame
+     * </p>
+     * @param rowIndex is the row to examine
+     * @return the row's expected outputs
+     */
     public ArrayList<Double> getRowExpected(int rowIndex) {
+        // Create a list
         ArrayList<Double> row = new ArrayList<>();
+        // For all columns
         for(int i = 0; i < numAttributes(); i++) {
+            // If the column is an output column
             if(outputCols.contains(i)) {
+                // Add the value to the row
                 row.add(dataFrame.get(i).get(rowIndex));
             }
         }
+        // Return the row
         return row;
     }
 
@@ -125,11 +174,27 @@ public abstract class Dataset {
         this.dataFrame = dataFrame;
     }
 
+    /**
+     * Function setInputCols()
+     * <p>
+     *     Set the inputCols to the passed list
+     * </p>
+     * @param colNums is the list to use for the inputCols
+     */
     public void setInputCols(ArrayList<Integer> colNums) {
+        // Set inputCols to passed value
         inputCols = colNums;
     }
 
+    /**
+     * Function setOutputCols()
+     * <p>
+     *     Set the outputCols to the passed list
+     * </p>
+     * @param colNums is the list to use for the outputCols
+     */
     public void setOutputCols(ArrayList<Integer> colNums) {
+        // Set outputCols to passed value
         outputCols = colNums;
     }
 }
