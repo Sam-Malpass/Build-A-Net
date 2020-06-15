cb0bb2db8751a6670f2fed36a09ae81146026a35
Sam <sam@malpass.me.uk>
2020-06-15 11:34:39 +0100

    Create Rounder class

---
+++ src/application/misc/Rounder.java
@@ -0,0 +1,25 @@
+/**
+ * Rounder
+ * @author Sam Malpass
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+package application.misc;
+
+import application.Main;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Rounder {
+    private static double round(double val, int places) {
+
+        if (places < 0) {
+            Main.passMessage("Error rounding number", "-e");
+            throw new IllegalArgumentException();
+        }
+
+        BigDecimal tmp = new BigDecimal(Double.toString(val));
+        tmp = tmp.setScale(places, RoundingMode.HALF_UP);
+        return tmp.doubleValue();
+    }
+}
